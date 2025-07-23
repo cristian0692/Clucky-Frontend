e7e55394afb6c7aff267b9b0fbc83b0f3865270f
Bolohan Cristian <cr.bolohan@gmail.com>
2025-07-23 21:26:32 +0300

    restaurant api

--- src/types.ts
+++ src/types.ts
@@ -25,3 +25,11 @@ export type Restaurant = {
     lastUpdated: string
 }
 
+export type RestaurantSearchResponse = {
+    data: Restaurant[];
+    pagination: {
+        total: number;
+        page: number;
+        pages: number;
+    }
+}
