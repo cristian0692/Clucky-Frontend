33c7e87a9f65b2bad295795bb3891a51c6e8e095
Bolohan Cristian <cr.bolohan@gmail.com>
2025-08-06 08:44:01 +0300

    Order Implementation

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, type AppState } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -15,10 +15,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialise auth");
   }
-  const onRedirectCallback = () => {
-    navigate("/auth-callback");
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || "/auth-callback");
   };
- return (
+  return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
